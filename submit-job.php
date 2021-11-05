47f1a4235f90e0da4c2c2413a5e67530b9a28d55
fran-fernan <93449150+fran-fernan@users.noreply.github.com>
2021-11-05 12:38:46 -0400

    omisgosh

--- submit-job.php
+++ submit-job.php
@@ -55,7 +55,7 @@ if ($jobs == "" && $other != "") {
 }
 */
 
-$random = 543;
+$random = "11052021";
 //$query = "insert into tasks (name, task, date, time_start, time_end) values ($name, $task, $date, $startTime, $endTime)";
 $query = "insert into tasks (name, task, date, time_start, time_end) values ($random, $task, $date, $startTime, $endTime)";
 $result = mysqli_query($conn, $query);
