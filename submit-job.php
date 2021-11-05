ab040c905fb9513f9849d0c6a73326a9a95ce39e
fran-fernan <93449150+fran-fernan@users.noreply.github.com>
2021-11-05 10:49:09 -0400

    query3

--- submit-job.php
+++ submit-job.php
@@ -48,7 +48,7 @@ if ($jobs == "" && $other != "") {
     }
 }
 */
-$query = "insert into tasks (name, task, date, time start, time end) values ('$name', '$task', '00-00-0000', '00:00', '00:00')";
+$query = "insert into tasks (name, task, date, time start, time end) values ('Hi', '2', '00-00-0000', '00:00:00', '00:00:00')";
 $result = mysqli_query($conn, $query);
 
 if ($result > 0)
