43206a21a8adda5dfea21aaed7ffc9a8bbd2c685
fran-fernan <93449150+fran-fernan@users.noreply.github.com>
2021-11-05 12:28:39 -0400

    new variable

--- submit-job.php
+++ submit-job.php
@@ -55,9 +55,9 @@ if ($jobs == "" && $other != "") {
 }
 */
 
-$name = "Test";
+$namer = "Test";
 //$query = "insert into tasks (name, task, date, time_start, time_end) values ($name, $task, $date, $startTime, $endTime)";
-$query = "insert into tasks (name, task, date, time_start, time_end) values ($name, $task, $date, $startTime, $endTime)";
+$query = "insert into tasks (name, task, date, time_start, time_end) values ($namer, $task, $date, $startTime, $endTime)";
 $result = mysqli_query($conn, $query);
 
 if ($result > 0)
