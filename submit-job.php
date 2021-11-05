467fcff75c74af46b54a2ac0954e79a57e681e7f
fran-fernan <93449150+fran-fernan@users.noreply.github.com>
2021-11-05 12:40:45 -0400

    why does it not accept letters

--- submit-job.php
+++ submit-job.php
@@ -55,7 +55,7 @@ if ($jobs == "" && $other != "") {
 }
 */
 
-$random = "11052021";
+$random = "110520d1";
 //$query = "insert into tasks (name, task, date, time_start, time_end) values ($name, $task, $date, $startTime, $endTime)";
 $query = "insert into tasks (name, task, date, time_start, time_end) values ($random, $task, $date, $startTime, $endTime)";
 $result = mysqli_query($conn, $query);
