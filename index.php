ad7eb37d138da172d2c81a1836735ea1302180ec
fran-fernan <93449150+fran-fernan@users.noreply.github.com>
2021-11-08 00:55:49 -0500

    4

--- index.php
+++ index.php
@@ -137,8 +137,8 @@
         
         require_once 'config.php';
 
-        $query = "SELECT * from tasks;";
-        $result = mysqli_query($conn, $query);
+        //$query = "SELECT * from tasks;";
+        //$result = mysqli_query($conn, $query);
         
         echo "<h6><table id='tasks' data-vertable='ver5' class="striped">";
         echo "<tr><th>Name</th>";
@@ -147,7 +147,7 @@
         echo "<th>Time End</th>";
         echo "<th>Time Total</th>";
 
-        echo "<tr><td><script>document.querySelector('input[name="name"]:checked').value;</script><td>";
+        //echo "<tr><td><script>document.querySelector('input[name="name"]:checked').value;</script><td>";
         //$query = "SELECT * FROM tasks WHERE name='$salarieid = $_POST['name'];'"
 
         /*
