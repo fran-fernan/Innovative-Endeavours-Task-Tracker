b0e1913c599f6c06e6ab53a2c116eaa3503d4c89
fran-fernan <93449150+fran-fernan@users.noreply.github.com>
2021-11-08 01:30:55 -0500

    14

--- index.php
+++ index.php
@@ -138,9 +138,9 @@
             $query = "SELECT * from tasks;";
             $result = mysqli_query($conn, $query);
 
-            echo "<div class="col s10">";
             echo "hello";
-            echo "</div>"
+            echo "<table id="tasks" data-vertable="ver5" class="striped">";
+            echo "</table>";
             /*
             echo "<h6><table id='tasks' data-vertable='ver5' class="striped">";
             echo "<tr><th>Name</th>";
