2e6b0119200b958703c70805c04a183a1b67d62f
fran-fernan <93449150+fran-fernan@users.noreply.github.com>
2021-11-12 17:16:07 -0500

    8

--- index.php
+++ index.php
@@ -157,7 +157,7 @@
                     echo "<td>" . $row["time_end"] . "</td>";
                     $time_diff = strtotime($row["time_end"]) - strtotime($row["time_start"]);
 
-                    echo "<td>" . $time_diff . "</td></tr>";
+                    echo "<td>" . gmdate("H:i:s", $time_diff) . "</td></tr>";
                 }
                 
                 echo "</div>";
