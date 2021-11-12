8430324ca1b0b5b76da5a0ad09da0fb1bee85d3c
fran-fernan <93449150+fran-fernan@users.noreply.github.com>
2021-11-12 15:32:44 -0500

    8

--- index.php
+++ index.php
@@ -139,8 +139,8 @@
                 $query = "SELECT * from tasks;";
                 $result = mysqli_query($conn, $query);
 
-                echo "<div>";
-                echo "<table width='200px'>";
+                echo "<div class='col 6'>";
+                echo "<table style='width:20%;'>";
                 echo "<tr><th>Date</th>";
                 echo "<th>Task</th>";
                 echo "<th>Time Start</th>";
