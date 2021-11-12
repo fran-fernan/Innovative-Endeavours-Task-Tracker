ae0238c3e62b6573a94b719f3ccf040e22493fb8
fran-fernan <93449150+fran-fernan@users.noreply.github.com>
2021-11-12 16:56:07 -0500

    12

--- index.php
+++ index.php
@@ -151,7 +151,7 @@
                 $query = "Select * from tasks where name='$name'";
                 $result = mysqli_query($conn, $query);
                 while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr style='background-color:#e1e1e1'><td>" . $row["date"] . "</td>";
+                    echo "<tr style='background-color:#f0f0f0'><td>" . $row["date"] . "</td>";
                     echo "<td>" . $row["task"] . "</td>";
                     echo "<td>" . $row["time_start"] . "</td>";
                     echo "<td>" . $row["time_end"] . "</td></tr>";
