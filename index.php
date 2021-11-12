9dda7b05a85f9d8dab5f0d80f8bb88b2b2af9396
fran-fernan <93449150+fran-fernan@users.noreply.github.com>
2021-11-12 15:15:26 -0500

    1

--- index.php
+++ index.php
@@ -140,7 +140,7 @@
                 $result = mysqli_query($conn, $query);
 
                 echo "<div>";
-                echo "<table>";
+                echo "<table style='width:50%;'>";
                 echo "<tr><th>Date</th>";
                 echo "<th>Time Start</th>";
                 echo "<th>Time End</th>";
